d11d3cc968d7c9f86ab353a934fd33082fa9ddaf
AndyNoob <comfortableandy9082@gmail.com>
2024-06-28 15:40:18 -0700

    Add action config loading

--- src/main/java/comfortable_andy/combat/CombatMain.java
+++ src/main/java/comfortable_andy/combat/CombatMain.java
@@ -1,24 +1,21 @@
 package comfortable_andy.combat;
 
-import comfortable_andy.combat.util.PlayerUtil;
+import comfortable_andy.combat.actions.IAction;
+import comfortable_andy.combat.actions.SweepAction;
 import io.papermc.paper.event.player.PrePlayerAttackEntityEvent;
 import lombok.Getter;
 import lombok.Setter;
-import org.bukkit.Location;
-import org.bukkit.entity.Damageable;
+import me.comfortable_andy.mapable.Mapable;
+import me.comfortable_andy.mapable.MapableBuilder;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.bukkit.util.Vector;
-import org.joml.Quaterniond;
 
-import java.util.Arrays;
-import java.util.Map;
-import java.util.Objects;
+import java.io.File;
+import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
-
-import static comfortable_andy.combat.util.VecUtil.fromDir;
 
 public final class CombatMain extends JavaPlugin implements Listener {
 
@@ -28,10 +25,13 @@ public final class CombatMain extends JavaPlugin implements Listener {
     @Setter
     @Getter
     private boolean debugLog = false;
+    private final List<IAction> actions = new ArrayList<>();
+    private final Mapable mapable = new MapableBuilder().createMapable();
 
     @Override
     public void onEnable() {
         INSTANCE = this;
+        loadActions();
         new CombatRunnable().runTaskTimer(this, 0, 1);
         getServer().getPluginManager().registerEvents(this, this);
     }
@@ -44,22 +44,8 @@ public final class CombatMain extends JavaPlugin implements Listener {
     public void onPlayerInteract(PrePlayerAttackEntityEvent event) {
         event.setCancelled(true);
         final Player player = event.getPlayer();
-        final Location origin = player.getEyeLocation();
-        final CombatPlayerData data = getData(player);
-        final Quaterniond delta = fromDir(data.averageCameraAngleDelta());
-        final Quaterniond start = fromDir(origin);
-        start.mul(delta.invert(new Quaterniond()));
-        for (Map.Entry<Damageable, Vector> entry :
-                PlayerUtil.sweep(
-                        origin,
-                        2,
-                        1,
-                        start,
-                        fromDir(data.averageCameraAngleDelta()),
-                        5
-                ).entrySet()) {
-            if (entry.getKey() == player) continue;
-            entry.getKey().teleport(entry.getKey().getLocation().add(entry.getValue()));
+        for (IAction action : actions) {
+            if (action.tryActivate(getData(player), IAction.ActionType.ATTACK) == IAction.ActionResult.ACTIVATED) break;
         }
     }
 
@@ -74,6 +60,55 @@ public final class CombatMain extends JavaPlugin implements Listener {
 
     public void purgeData() {
         playerData.entrySet().removeIf(d -> !d.getValue().getPlayer().isOnline());
+    }
+
+    public void loadActions() {
+        saveResource("actions/sweep.yml", false);
+        try {
+            actions.add(mapable.fromMap(configToMap(YamlConfiguration.loadConfiguration(new File(getDataFolder(), "actions/sweep.yml"))), SweepAction.class));
+        } catch (ReflectiveOperationException e) {
+            throw new RuntimeException(e);
+        }
+        getLogger().info("Loaded " + actions);
+    }
+
+    @SuppressWarnings("unchecked")
+    public Map<String, Object> configToMap(YamlConfiguration config) {
+        final Map<String, Object> map = new HashMap<>();
+        final Map<List<String>, Set<String>> toRead = new ConcurrentHashMap<>();
+        toRead.put(new ArrayList<>(), config.getKeys(false));
+        while (!toRead.isEmpty()) {
+            for (Iterator<Map.Entry<List<String>, Set<String>>> iterator = toRead.entrySet().iterator(); iterator.hasNext(); ) {
+                Map.Entry<List<String>, Set<String>> e = iterator.next();
+
+                final List<String> path = e.getKey();
+                final Set<String> keys = e.getValue();
+
+                Map<String, Object> addingTo = path.isEmpty() ? map : (Map<String, Object>) map
+                        .computeIfAbsent(path.get(0), k -> new HashMap<>());
+
+                for (int i = 1; i < path.size(); i++) {
+                    final String s = path.get(i);
+                    addingTo = (Map<String, Object>) addingTo
+                            .computeIfAbsent(s, k -> new HashMap<>());
+                }
+
+                for (String key : keys) {
+                    final List<String> newPath = new ArrayList<>(path);
+                    newPath.add(key);
+                    final String concatNewPath = String.join(".", newPath);
+                    if (config.isConfigurationSection(concatNewPath)) {
+                        toRead.computeIfAbsent(newPath, k -> new HashSet<>()).addAll(
+                                Objects.requireNonNull(config.getConfigurationSection(concatNewPath)).getKeys(false)
+                        );
+                    } else {
+                        addingTo.put(key, config.get(concatNewPath));
+                    }
+                }
+                iterator.remove();
+            }
+        }
+        return map;
     }
 
 }
