f3fda9104e09b56c93400772550247088da6b67f
AndyNoob <comfortableandy9082@gmail.com>
2024-07-13 17:20:54 -0700

    Lower obb display length

--- src/main/java/comfortable_andy/combat/util/OrientedBox.java
+++ src/main/java/comfortable_andy/combat/util/OrientedBox.java
@@ -132,7 +132,7 @@ public class OrientedBox implements Cloneable {
 
     public void display(World world) {
         new BukkitRunnable() {
-            int count = 2;
+            int count = 0;
 
             @Override
             public void run() {
