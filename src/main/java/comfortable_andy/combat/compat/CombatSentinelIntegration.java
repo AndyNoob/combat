f4b4e28e816252c7ec033d4e37f04bf08e358ca1
AndyNoob <comfortableandy9082@gmail.com>
2024-09-07 16:19:58 -0700

    Continue close chase based on target reach

--- src/main/java/comfortable_andy/combat/compat/CombatSentinelIntegration.java
+++ src/main/java/comfortable_andy/combat/compat/CombatSentinelIntegration.java
@@ -81,8 +81,9 @@ public class CombatSentinelIntegration extends SentinelIntegration {
         combatData.getOptions().compensateCameraMovement(false);
         st.faceLocation(ent.getEyeLocation());
         st.attackHelper.rechase();
+        final double reach = ent instanceof Player pl ? PlayerUtil.getReach(pl) : st.reach;
         if (player.getEyeLocation()
-                .distanceSquared(ent.getEyeLocation()) > st.reach * st.reach) {
+                .distanceSquared(ent.getEyeLocation()) > reach * reach) {
             if (!st.rangedChase) {
                 npc.getNavigator().cancelNavigation();
             }
